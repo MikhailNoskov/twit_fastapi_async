54b2e83168b7f27aaccd4618aa8f15be77671d4a
flyingmike <noskovmm@gmail.com>
2022-10-30 21:41:38 +0100

    Update: - app, database, models initially added

---
+++ src/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    is_active = Column(Boolean, default=False)
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user = Column(forein_key=User)
+    text = Column(String)
+    created_at = Column(...)
+
+
+class Reaction(Base):
+    __tablename__ = "reactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user = Column(forein_key=User)
+    message = Column(foreign_key=Message)
+    text = Column(String)
+    created_at = Column(...)
+
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user = Column(forein_key=User)
+    folooewd = Column(forein_key=User)
+    created_at = Column(...)
+    is_active = Column(Boolean, default=False)
