55c4f31b9033acfe9027701fd77a511f5be6ec12
flyingmike <noskovmm@gmail.com>
2022-10-31 15:16:24 +0100

    Update: - endpoint added to app

--- src/app.py
+++ src/app.py
@@ -23,38 +23,81 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def home(request: Request, db: Session = Depends(get_db)):
-    todos = db.query(models.Todo).all()
-    return templates.TemplateResponse("base.html",
-                                      {"request": request, "todo_list": todos})
+@app.get('/api/users/me')
+async def me(request: Request, db: Session = Depends(get_db)):
+    pass
 
 
-@app.post("/add")
-async def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
-    new_todo = models.Todo(title=title)
-    db.add(new_todo)
-    db.commit()
-
-    url = app.url_path_for("home")
-    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+@app.get('/api/users/{user_id}')
+async def user(request: Request, db: Session = Depends(get_db)):
+    pass
 
 
-@app.get("/update/{todo_id}")
-async def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
-    todo.complete = not todo.complete
-    db.commit()
-
-    url = app.url_path_for("home")
-    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+@app.post('/api/tweets')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
 
 
-@app.get("/delete/{todo_id}")
-async def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
-    db.delete(todo)
-    db.commit()
+@app.delete('/api/tweets/{tweet_id}')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
 
-    url = app.url_path_for("home")
-    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+@app.post('/api/medias')
+async def post_image(request: Request, db: Session = Depends(get_db)):
+    pass
+
+
+@app.post('/api/tweets/{tweet_id}/likes')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
+
+
+@app.delete('/api/tweets/{tweet_id}/likes')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
+
+
+@app.post('/api/users/{user_id}/follow')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
+
+
+@app.delete('/api/users/{user_id}/follow')
+async def send_tweet(request: Request, db: Session = Depends(get_db)):
+    pass
+# @app.get("/")
+# async def home(request: Request, db: Session = Depends(get_db)):
+#     todos = db.query(models.Smth).all()
+#     return templates.TemplateResponse("base.html",
+#                                       {"request": request, "todo_list": todos})
+#
+#
+# @app.post("/add")
+# async def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+#     new_todo = models.Smth(title=title)
+#     db.add(new_todo)
+#     db.commit()
+#
+#     url = app.url_path_for("home")
+#     return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+#
+#
+# @app.get("/update/{todo_id}")
+# async def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+#     smth = db.query(models.Smth).filter(models.Smth.id == todo_id).first()
+#     smth.complete = not smth.complete
+#     db.commit()
+#
+#     url = app.url_path_for("home")
+#     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+#
+#
+# @app.get("/delete/{todo_id}")
+# async def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+#     smth = db.query(models.Smth).filter(models.smth.id == todo_id).first()
+#     db.delete(smth)
+#     db.commit()
+#
+#     url = app.url_path_for("home")
+#     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
