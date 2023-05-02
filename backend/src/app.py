6af0b760442860fc43f0103dcd4da6a90756d63e
Mikhail Noskov <noskovmm@gmail.com>
2023-05-02 22:17:32 +0200

    on_event commented

--- backend/src/app.py
+++ backend/src/app.py
@@ -11,21 +11,21 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 
 
-@app.on_event("startup")
-async def startup_event():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    async with async_session() as session:
-        async with session.begin():
-            session.add_all(
-                [
-                    User(username='Mike', password='mike_pass'),
-                    User(username='Alla', password='alla_pass'),
-                    Tweet(text='Hey', comment='Haya')
-                ]
-            )
+# @app.on_event("startup")
+# async def startup_event():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
+#
+#     async with async_session() as session:
+#         async with session.begin():
+#             session.add_all(
+#                 [
+#                     User(username='Mike', password='mike_pass'),
+#                     User(username='Alla', password='alla_pass'),
+#                     Tweet(text='Hey', comment='Haya')
+#                 ]
+#             )
 
 
 
