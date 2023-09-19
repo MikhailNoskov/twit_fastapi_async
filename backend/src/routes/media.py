b9a9d03f696ddd28ba6bfa2f5e6e1ab71c7b2090
Mikhail Noskov <noskovmm@gmail.com>
2023-09-19 19:29:00 +0200

    user injection in media endpoint

--- backend/src/routes/media.py
+++ backend/src/routes/media.py
@@ -1,18 +1,12 @@
 import shutil
 import string
 import random
-from typing import Optional, List
 
-from fastapi import APIRouter, HTTPException, status, Header, Depends, UploadFile, File
-from models.media import Media
-from models.users import User
+from fastapi import APIRouter, HTTPException, status, UploadFile, File
+from fastapi.requests import Request
+
 from schema.media import MediaResponse
-from database.connection import get_session
 from database.media import file_db_record
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload, selectinload, subqueryload, aliased
-from fastapi.responses import JSONResponse
 
 
 media_router = APIRouter(
@@ -20,20 +14,14 @@ media_router = APIRouter(
 )
 
 
-async def verify_api_key(api_key: Optional[str] = Header(), db: AsyncSession = Depends(get_session)):
-    async with db.begin():
-        user = await db.execute(select(User).where(User.api_key == api_key))
-        user = user.scalar_one_or_none()
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="User not found"
-            )
-        return True
-
-
-@media_router.post('/', dependencies=[Depends(verify_api_key)], response_model=MediaResponse)
-async def post_new_media_file(file: UploadFile = File(...), db: AsyncSession = Depends(get_session)):
+@media_router.post('/', response_model=MediaResponse)
+async def post_new_media_file(request: Request, file: UploadFile = File(...)):
+    user = request.state.user
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access denied"
+        )
     letters = string.ascii_letters
     random_str = ''.join(random.choice(letters) for i in range(6))
     new = f'_{random_str}.'
