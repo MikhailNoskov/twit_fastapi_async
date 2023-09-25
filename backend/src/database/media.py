be11cc112a449fc596ee887f593b244fa7ab4125
Mikhail Noskov <noskovmm@gmail.com>
2023-09-25 21:04:37 +0200

    Services for Media and Tweets added

--- backend/src/database/media.py
+++ backend/src/database/media.py
@@ -1,9 +1,11 @@
 import logging.config
 
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from models.media import Media
 from schema.media import MediaResponse
-from database.connection import async_session_maker as session
-
+from database.connection import get_session
 from logging_conf import logs_config
 
 logging.config.dictConfig(logs_config)
@@ -11,13 +13,17 @@ logger = logging.getLogger("app.db_media")
 logger.setLevel("DEBUG")
 
 
-async def file_db_record(path: str):
-    async with session() as db:
-        async with db.begin():
+class MediaService:
+    def __init__(self, session: AsyncSession = Depends(get_session)):
+        self.session = session
+
+    async def file_db_record(self, path: str):
+        # async with session() as db:
+        async with self.session.begin():
             new_media = Media(media_url=path)
-            db.add(new_media)
-            await db.flush()
-            await db.refresh(new_media)
+            self.session.add(new_media)
+            await self.session.flush()
+            await self.session.refresh(new_media)
             image = MediaResponse(media_id=new_media.id)
             logger.debug(msg=f'Image {path} added')
             return image
