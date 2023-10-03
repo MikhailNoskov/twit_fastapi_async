c5a7558aad9123f15b3983aa2d258d1b635096e4
Mikhail Noskov <noskovmm@gmail.com>
2023-10-04 00:46:45 +0200

    Big images lazy_loading

--- backend/src/database/media.py
+++ backend/src/database/media.py
@@ -1,14 +1,17 @@
 import logging.config
 
+from fastapi import HTTPException
 from models.media import Media
 from schema.media import MediaResponse
 from logging_conf import logs_config
+from database.connection import async_session_maker
 from database.services import AbstractService
-
+from sqlalchemy import select
 
 logging.config.dictConfig(logs_config)
 logger = logging.getLogger("app.db_media")
 logger.setLevel("DEBUG")
+
 
 
 class MediaService(AbstractService):
@@ -29,3 +32,21 @@ class MediaService(AbstractService):
             image = MediaResponse(media_id=new_media.id)
             logger.debug(msg=f'Image {path} added')
             return image
+
+async def update_media_path(media_id: int, new_path: str) -> None:
+    """
+
+    :param media_id:
+    :param new_path:
+    :return:
+    """
+    async with async_session_maker() as db:
+        async with db.begin():
+            media = await db.execute(select(Media).where(Media.id == media_id))
+            media = media.scalars().first()
+            if not media:
+                raise HTTPException(status_code=404, detail="Media not found")
+            media.media_url = new_path
+            await db.merge(media)
+            await db.commit()
+            logger.info(msg='New path is saved')
--- backend/src/models/media.py
+++ backend/src/models/media.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from database.connection import Base
-
+from models.tweets import Tweet
 
 class Media(Base):
     """
@@ -13,7 +13,7 @@ class Media(Base):
     id = Column(Integer, primary_key=True, index=True)
     media_url = Column(String(500), nullable=False)
     tweet_id = Column(Integer, ForeignKey('tweets.id'), nullable=True)
-    tweet = relationship('Tweet', back_populates='attachments')
+    tweet = relationship('Tweet', back_populates='attachments', lazy="select")
 
     def __repr__(self):
         return f"File Nr{self.id}"
