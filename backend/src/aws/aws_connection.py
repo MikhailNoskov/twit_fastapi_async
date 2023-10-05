dcd891fc7b70529e84c7f73af48fcfe456330400
Mikhail Noskov <noskovmm@gmail.com>
2023-10-05 23:24:05 +0200

    aws credentials updated

--- backend/src/aws/aws_connection.py
+++ backend/src/aws/aws_connection.py
@@ -1,7 +1,7 @@
 import boto3
 
 s3 = boto3.client('s3',
-                  aws_access_key_id='AKIAWOQ5PH2LOOZZ5FFR',
+                  aws_access_key_id='AKIAWOQ5PH2LBIBBMO54',
                   # aws_secret_access_key='123'
-                  aws_secret_access_key='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'
+                  aws_secret_access_key='hnkVyJIM/+FthLHFIcGQuTaW8ekZRP/VKQj8KkP9'
                   )
