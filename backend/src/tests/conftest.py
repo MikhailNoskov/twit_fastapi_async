f8d9b5837df3ce250b620a53b0e7a7a420f1962c
Mikhail Noskov <noskovmm@gmail.com>
2023-10-17 10:44:45 +0200

    test for follow/unfollow with different combinations added

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -1,9 +1,13 @@
 import pytest
 import asyncio
 
+from sqlalchemy import select, insert, column, delete
+
 from database.connection import Base, get_session, engine, async_session_maker
 from auth.hash_password import HashPassword
-from models.users import User
+from models.users import User, users_connections
+from models.tweets import Tweet, Like
+from tests.data_to_upload import users
 
 
 @pytest.fixture(scope="session")
@@ -33,14 +37,13 @@ async def create_data(drop_base):
     await drop_base
     async with async_session_maker() as session:
         async with session.begin():
-            users = {
-                0: ['mike@klike.com', 'pass_1', 'test'],
-                1: ['jorge@klike.com', 'pass_2', 'rest'],
-                2: ['rob@klike.com', 'pass_3', 'nest'],
-            }
             for email, password, api_key in users.values():
                 hashed_password = HashPassword.create_hash(password)
                 user = User(name=email, password=hashed_password, api_key=api_key)
                 session.add(user)
                 await session.flush()
                 await session.refresh(user)
+            stmt = insert(users_connections).values(follower_id=1, followed_id=2)
+            await session.execute(stmt)
+            await session.commit()
+
---
+++ backend/src/tests/data_to_upload.py
@@ -0,0 +1,6 @@
+users = {
+    0: ['mike@klike.com', 'pass_1', 'test'],
+    1: ['jorge@klike.com', 'pass_2', 'rest'],
+    2: ['rob@klike.com', 'pass_3', 'nest'],
+}
+
