03b08110100ac523feaa15e7129b30f4b80ee044
Mikhail Noskov <noskovmm@gmail.com>
2023-10-17 11:16:15 +0200

    tests for media loaded by not authenticated user added

--- backend/src/tests/test_media.py
+++ backend/src/tests/test_media.py
@@ -9,8 +9,8 @@ from main import app
 @pytest.mark.asyncio
 async def test_post_media(create_data):
     """
-    Post new tweet test function
-    :param test_app: App instance
+    Post new media test function
+    :param create_data: db drop and create fixture function
     :return: None
     """
     await create_data
@@ -24,3 +24,22 @@ async def test_post_media(create_data):
     response = await client.post("/api/medias/", files=file, headers=headers)
     assert response.json() == expected
     assert response.status_code == 201
+
+
+@pytest.mark.asyncio
+async def test_post_media_not_authenticated(create_data):
+    """
+    Post new media by not authenticated user test function
+    :param create_data: db drop and create fixture function
+    :return: None
+    """
+    fake_img = Image.new("RGB", size=(100, 100))
+    img_bytes = BytesIO()
+    fake_img.save(img_bytes, format="JPEG")
+    client = AsyncClient(app=app, base_url="http://test")
+    expected = {'error_message': 'Access denied', 'error_type': 'medias', 'result': False}
+    file = {"file": ("test_file.jpg", img_bytes.getvalue())}
+    headers = {"api-key": "west"}
+    response = await client.post("/api/medias/", files=file, headers=headers)
+    assert response.json() == expected
+    assert response.status_code == 403
